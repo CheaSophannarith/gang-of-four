4d3e4a8b70c8e27e00b91d031855a33d90c09529
Channsoriya <chann.soriya.1221@rupp.edu.kh>
2025-10-23 15:31:55 +0700

    Refactor layout and page components; update TypeScript configuration for improved structure and font usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import font from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const libreBaskerville = Libre_Baskerville({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-libre-baskerville",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={libreBaskerville.className}
       >
         {children}
       </body>
